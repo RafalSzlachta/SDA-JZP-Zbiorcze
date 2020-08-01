c8d65465bbbe42062a7d7c63d22f1f70bb446c0c
RafalSzlachta <rszlachta@o2.pl>
2020-08-01 20:24:46 +0200

    corrected ex1

--- src/test/java/pl/sda/ex1/StringServiceTest.java
+++ src/test/java/pl/sda/ex1/StringServiceTest.java
@@ -2,7 +2,6 @@ package pl.sda.ex1;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,8 +11,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringServiceTest {
     @Test
     public void testGetSortedAlphabeticllyZAShouldSortCorrectly(){
-        List<String> listUnsorted = new ArrayList<>();
-        getUnsortedStrings(listUnsorted);
+        List<String> listUnsorted = getUnsortedStrings();
+        ;
 
         StringService ss = new StringService();
         List<String> listSorted = new ArrayList<>();
@@ -27,21 +26,21 @@ public class StringServiceTest {
 
     @Test
     public void testGetSortedAlphabeticllyZACaseInsensitiveShouldSortCorrectly(){
-        List<String> listUnsorted = new ArrayList<>();
-        getUnsortedStrings(listUnsorted);
+        List<String> listUnsorted = getUnsortedStrings();
 
         StringService ss = new StringService();
         List<String> listSorted = new ArrayList<>();
         listSorted = ss.getSortedAlphabeticallyZACaseIntensitive(listUnsorted);
         System.out.println(listSorted);
 
-        List<String> manuallyListSorted = Arrays.asList("za","ab","Ab","aB","aa");
+        List<String> manuallyListSorted = Arrays.asList("za","Ab","aB","ab","aa");
 
         assertEquals(manuallyListSorted, listSorted);
     }
 
     //coś nie działa, porównaj z kodem trenera
-    private List<String> getUnsortedStrings(List<String> listUnsorted) {
+    private List<String> getUnsortedStrings() {
+        List<String> listUnsorted = new ArrayList<>();
         listUnsorted.add("za");
         listUnsorted.add("Ab");
         listUnsorted.add("aa");
