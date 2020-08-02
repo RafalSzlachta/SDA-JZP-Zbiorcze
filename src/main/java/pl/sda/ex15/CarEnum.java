ce98493f5ef20ccaf2aa5d06dc1219a18ba9a0fe
RafalSzlachta <rszlachta@o2.pl>
2020-08-02 12:26:52 +0200

    solved ex15

---
+++ src/main/java/pl/sda/ex15/CarEnum.java
@@ -0,0 +1,39 @@
+package pl.sda.ex15;
+
+import java.security.PublicKey;
+
+public enum CarEnum {
+    FERRARI(100.0d, true),
+    BMW(80.0d, true),
+    OPEL(60.0d, false);
+
+    private Double velocity;
+    private boolean premium;
+
+    CarEnum(double velocity, boolean premium) {
+        this.velocity = velocity;
+        this.premium = premium;
+    }
+
+    public boolean isPremium() {
+        return premium;
+    }
+
+    public void setPremium(boolean premium) {
+        this.premium = premium;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(double velocity) {
+        this.velocity = velocity;
+    }
+
+    public boolean isFasterThan(CarEnum car){
+        return this.velocity.compareTo(car.velocity)>0;
+    }
+
+
+}
